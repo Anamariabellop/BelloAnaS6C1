e63112c3cf3229f65387e0364236b5b09c0401c0
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-09 09:42:14 -0500

    No da bien

--- BelloAnaMS6C1.cpp
+++ BelloAnaMS6C1.cpp
@@ -8,11 +8,9 @@ void adveccion(float t0, float tf,float constante, int npuntos, string filename)
 int main(){
 
 	float v=1.0;
-	int N=80.0;
-	float dx=0.01;
-	float dt= dx/v;
-	float c= (v*dt)/dx;
-	adveccion(0.0,2.0, c, N, "pulso.txt");
+	int N=80.0;	
+	
+	adveccion(0.0,2.0,v, N, "pulso.txt");
 	return 0;
 }
 
@@ -20,18 +18,22 @@ void adveccion(float t0, float tf,float constante, int npuntos, string filename)
 	ofstream outfile;
 	outfile.open(filename);
 
+
 	double dx= (tf- t0)/(npuntos-1);
-	float u[npuntos][npuntos];
+	float dt=(dx*0.1)/constante ;
+	//float w= (constante*dt)/dx;
+	cout << dx << endl;
+	//float u[npuntos];
 	float ufuturo[npuntos];
 	float upasado[npuntos];
 	float amplitud[npuntos];
 	amplitud[0]=0;
-	
 
 	for(int i=1; i<npuntos ; i++)// para crear linspace con npuntos determinado
     {  
       amplitud[i]= amplitud[i-1]+dx;
     }
+
     for(int i=0; i< npuntos; i++){
     	if(amplitud[i]<0.75){
     		upasado[i]=1.0;
@@ -44,8 +46,13 @@ void adveccion(float t0, float tf,float constante, int npuntos, string filename)
     		upasado[i]=2.0;
     	}
     }
+
+    for(int i=0; i < npuntos; i++){
+    	ufuturo[i]= constante*(dt/dx)*(upasado[i]-upasado[i-1])+upasado[i];
+    }
+
  	for (int i=0; i<npuntos; i++){
- 		outfile <<  amplitud[i] <<"  "<< upasado[i] << endl;
+ 		outfile<< amplitud[i] <<"  "<< upasado[i] << "  " << ufuturo[i] << endl;
  	}
     outfile.close();
 }
