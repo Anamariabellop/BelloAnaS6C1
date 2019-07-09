baa6c7201be4e3bed7c72064f33058f04fdb2db9
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-09 09:42:54 -0500

    grafica

--- S6C1plot.py
+++ S6C1plot.py
@@ -4,12 +4,25 @@ import matplotlib.pylab as plt
 datos= np.genfromtxt("pulso.txt")
 
 t=datos[:,0]
-x=datos[:,1]
+upasado=datos[:,1]
+ufuturo=datos[:,2]
 
 plt.figure()
-plt.plot(t,x, color= 'c', label='upasado')
+
+plt.subplot(2,1,1)
+plt.plot(t,upasado, color= 'c', label='upasado')
 plt.xlabel("Amplitud")
 plt.ylabel(" ")
-plt.title("Condiciones iniciales")
 plt.legend()
+plt.grid()
+plt.title("Condiciones iniciales")
+
+plt.subplot(2,1,2)
+plt.plot(t,ufuturo, color= 'g', label='ufuturo')
+plt.xlabel("Amplitud")
+plt.ylabel(" ")
+plt.legend()
+plt.grid()
+plt.title("Condiciones finales")
+plt.subplots_adjust(hspace=.5)
 plt.savefig("pulso.png")
